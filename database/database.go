e68fbcb015aa61b73460f2395dc45d380d920a5c
Phumai Ruangchit <Phumai.Ru@cjexpress.co.th>
2025-03-03 15:12:15 +0700

    used env

--- database/database.go
+++ database/database.go
@@ -2,13 +2,31 @@ package database
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectDatabase() *gorm.DB {
-	dsn := "root:1q2w3e4r@tcp(localhost:3307)/web_promotion_dev?charset=utf8mb4&parseTime=True&loc=Local"
+	// โหลดค่าจากไฟล์ .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// ดึงค่าจาก Environment Variables
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	// สร้าง DSN
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPass, dbHost, dbPort, dbName)
 
 	// เปิดการเชื่อมต่อฐานข้อมูล
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
